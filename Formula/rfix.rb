e0da9d431f8011e7dcdff07c5e2aa9770891a02b
Balasubramanian Narasimhan <b.naras@gmail.com>
2020-02-11 11:14:20 -0800

    Update rfix.rb

--- Formula/rfix.rb
+++ Formula/rfix.rb
@@ -1,4 +1,4 @@
-class Rfix < Formula
+class RFIX < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
   url "http://cran.r-project.org/src/base/R-3/R-3.6.2.tar.gz"
